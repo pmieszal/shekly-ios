836d747da9c9f35ed34969de760c54366dce9009
Patryk Mieszała <patryk.mieszala@gmail.com>
2020-04-19 19:01:06 +0200

    - rename SPMPackages to Vendors - use cocoapods for Realm

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -5,7 +5,14 @@ if [[ $? != 0 ]] ; then
 fi
 
 brew install mint
+brew list cocoapods || brew install cocoapods
+
 rm -r Shekly-generated.xcodeproj
+rm -r Shekly-generated.xcworkspace
+
 mint run yonaskolb/XcodeGen@2.13.1 xcodegen
 mint install mac-cain13/R.swift@v5.1.0
 mint install realm/SwiftLint@0.39.1
+pod install
+
+open Shekly-generated.xcworkspace
